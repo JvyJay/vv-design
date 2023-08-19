7d26ccd9174aca864090fac6714b860907bb03e3
Jadon Guzman <jjkolet25@tutanota.com>
2023-08-19 14:26:58 -0700

    responsive menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import { Flex, Box, Heading, Link, Image } from '@chakra-ui/react';
+import { Box, Heading, Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
-import logo from '../img/1.png'
+import logo from '../img/1.png';
 
 const Navbar = () => {
   return (
     <Box bg="teal.500" p={4}>
-      <Flex align="center" maxW="1200px" mx="auto">
-        <Heading as="image" size="md" color="white" mr={8}>
-          <Image src={logo} alt="Logo" boxSize="40px" mr={8}>
+      <Box maxW="1200px" mx="auto">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Heading as="h1" size="md" color="white">
+            <RouterLink to="/">
+              <img src={logo} alt="Logo" style={{ height: '40px', marginRight: '8px' }} />
+            </RouterLink>
+          </Heading>
 
-          </Image>
-        </Heading>
-        <Link as={RouterLink} to="/" color="white" mr={4}>
-          Home
-        </Link>
-        <Link as={RouterLink} to="/about" color="white" mr={4}>
-          About
-        </Link>
-        <Link as={RouterLink} to="/services" color="white" mr={4}>
-          Services
-        </Link>
-        <Link as={RouterLink} to="/contact" color="white" mr={4}>
-          Contact
-        </Link>
-      </Flex>
+          <Menu>
+            <MenuButton as={Button} variant="link" color="white">
+              Menu
+            </MenuButton>
+            <MenuList>
+              <MenuItem as={RouterLink} to="/" color="teal.500">
+                Home
+              </MenuItem>
+              <MenuItem as={RouterLink} to="/about" color="teal.500">
+                About
+              </MenuItem>
+              <MenuItem as={RouterLink} to="/services" color="teal.500">
+                Services
+              </MenuItem>
+              <MenuItem as={RouterLink} to="/contact" color="teal.500">
+                Contact
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Box>
     </Box>
   );
 };
