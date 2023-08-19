107a1200ed2e14a0525256bd43c56a277404bf8a
Jadon Guzman <jjkolet25@tutanota.com>
2023-08-19 14:13:30 -0700

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Flex, Box, Heading, Link } from '@chakra-ui/react';
+import { Flex, Box, Heading, Link, Image } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
+import logo from '../img/1.png'
 
 const Navbar = () => {
   return (
     <Box bg="teal.500" p={4}>
       <Flex align="center" maxW="1200px" mx="auto">
-        <Heading as="h1" size="md" color="white" mr={8}>
-          Logo
+        <Heading as="image" size="md" color="white" mr={8}>
+          <Image src={logo} alt="Logo" boxSize="40px" mr={8}>
+
+          </Image>
         </Heading>
         <Link as={RouterLink} to="/" color="white" mr={4}>
           Home
