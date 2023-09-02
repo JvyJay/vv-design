253ce3109224760f58ae6a5d297ca85e1a5826d6
Jadon Guzman <jjkolet25@tutanota.com>
2023-09-02 16:12:02 -0700

    form validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,27 +1,63 @@
 import React from 'react';
-import { Box, Button, FormControl, FormLabel, Input, Textarea, VStack } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const ContactForm = () => {
   return (
     <Box mb={6} p={6} bg="white" borderRadius="md" boxShadow="md">
-      <VStack spacing={4}>
-        <FormControl id="name">
-          <FormLabel>Name</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="email">
-          <FormLabel>Email</FormLabel>
-          <Input type="email" />
-        </FormControl>
-        <FormControl id="message">
-          <FormLabel>Message</FormLabel>
-          <Textarea rows={4} />
-        </FormControl>
-        <Button colorScheme="teal">Send Message</Button>
-      </VStack>
+      <Formik
+        initialValues={{ name: '', email: '', message: '' }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('Required'),
+          email: Yup.string().email('Invalid email address').required('Required'),
+          message: Yup.string().required('Required'),
+        })}
+        onSubmit={(values, { resetForm }) => {
+          // Handle form submission here (e.g., send data to backend)
+          console.log(values);
+          // Reset the form after submission
+          resetForm();
+        }}
+      >
+        <Form>
+          <Field name="name">
+            {({ field }) => (
+              <FormControl id="name" isInvalid={field.value && field.value.length === 0}>
+                <FormLabel>Name</FormLabel>
+                <Input {...field} />
+                <ErrorMessage style={{ color: 'red' }} name="name" component="div" color="red.500" />
+              </FormControl>
+            )}
+          </Field>
+
+          <Field name="email">
+            {({ field }) => (
+              <FormControl id="email" isInvalid={field.value && !/^\S+@\S+\.\S+$/.test(field.value)}>
+                <FormLabel>Email</FormLabel>
+                <Input {...field} />
+                <ErrorMessage style={{ color: 'red' }} name="email" component="div" color="red.500" />
+              </FormControl>
+            )}
+          </Field>
+
+          <Field name="message">
+            {({ field }) => (
+              <FormControl id="message" isInvalid={field.value && field.value.length === 0}>
+                <FormLabel>Message</FormLabel>
+                <Textarea {...field} />
+                <ErrorMessage style={{ color: 'red' }} name="message" component="div" color="red.500" />
+              </FormControl>
+            )}
+          </Field>
+
+          <Button type="submit" colorScheme="teal" mt={4}>
+            Send Message
+          </Button>
+        </Form>
+      </Formik>
     </Box>
   );
 };
 
 export default ContactForm;
-
